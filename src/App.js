4e11a0bb30ebb6df8bb9f0e33dbd15e33bd14c5d
ruizaj13 <ruizaj13@gmail.com>
2020-11-04 20:44:12 -0800

    api is now working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import "./App.css";
 import axios from 'axios';
+import Header from './components/Header';
+import ImageCard from './components/ImageCard';
 
 function App() {
+  const [nasaData, setNasaData] = useState();
 
-
+  const fetchData = useEffect(() => {
+    axios 
+    .get('https://api.nasa.gov/planetary/apod?api_key=RhRYFjGb5MCLuF7CIhBeJxRDXuaR81VyTtmGCTTl&date=2020-11-04')
+    .then( res => {
+      console.log(res.data);
+    })
+    .catch(err => {
+      console.log('error', err)
+    })
+  }, []);
 
   return (
     <div className="App">
